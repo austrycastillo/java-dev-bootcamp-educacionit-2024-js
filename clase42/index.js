16b050ecddcd45de9478f94825aee694dc5ff5c9
Austry <castilloaustry@gmail.com>
2024-08-19 21:54:25 -0300

    clase 42: spa, localstorage

---
+++ clase42/index.js
@@ -0,0 +1,72 @@
+//SPA
+// let contenido = document.getElementById("contenido");
+// let links = document.querySelectorAll("a");
+// links.forEach(link => {
+//     link.addEventListener("click", e => {
+//         e.preventDefault();
+//         // console.log(e)
+//         // console.log(e.target)
+//         // console.log(e.target.href)
+//         let datos = e.target.href;
+//         fetch(datos)
+//             .then(response => response.text())
+//             .then(conten => {
+//                 // console.log(conten)
+//                 contenido.innerHTML = conten;
+//                 history.pushState(conten, "index", datos);
+//             })
+//     });
+// });
+
+// window.addEventListener("popstate", e => {
+//     console.log(e.state);
+//     contenido.innerHTML = e.state;
+// });
+
+
+
+//SPA CON EVENTOS
+// const a = document.querySelectorAll("a");
+// a.forEach(link => {
+//     console.log(link.classList[0]);
+//     let clase = link.classList[0];//
+//     link.addEventListener("click", e => {
+//         e.preventDefault();
+//         console.log(e);
+//         console.log(e.target);
+//         console.log(e.target.class);
+//         // history.pushState({
+//         //     key: clase
+//         // }, "", clase);
+//         let span = document.querySelectorAll("span");
+//         span.forEach(info => {
+//             console.log(info);
+//             console.log(info.id);
+//             let id = info.id;//
+//             if (clase == id) {
+//                 info.classList.remove("hide");
+//                 info.classList.add("active");
+//             } else {
+//                 info.classList.remove("active");
+//                 info.classList.add("hide");
+//             }
+//         });
+//     });
+// });
+
+
+//LOCALSTORAGE
+//MÉTODOS: setItem, getItem, clear, removeItem
+function registrarLocal() {
+    // console.log(document.form)
+    let fruta = document.getElementById("fruta");
+    let dulce = document.getElementById("dulce");
+    document.form.fruta.value
+    localStorage.setItem("fruta", document.form.fruta.value);
+    localStorage.setItem("dulce", document.form.dulce.value);
+}
+
+document.querySelector("a").addEventListener("click", e => {
+    e.preventDefault();
+    console.log(localStorage.getItem(fruta));
+});
